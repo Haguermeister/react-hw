37e68c9584404487f51a84f8d83ab372aa20089c
Haguermeister <cadethague70@gmail.com>
2022-04-11 17:05:34 -0400

    About tests using jest

---
+++ photo-port/src/components/About/__tests__/index.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from "..";
+
+describe('About component', () => {
+    //renders about test
+    it('renders', () => {
+        render(<About />);
+    });
+    // render about
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+
+})
